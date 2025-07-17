fc9a9944695abe1770dcc2ee5d3560186277f825
sdw98 <sdw9851@gmail.com>
2025-07-17 13:35:56 +0900

    AddIngredient

--- src/main/java/com/sdw/recipe_platform/controller/RecipeController.java
+++ src/main/java/com/sdw/recipe_platform/controller/RecipeController.java
@@ -1,14 +1,13 @@
 package com.sdw.recipe_platform.controller;
 
+import com.sdw.recipe_platform.dto.AddIngredientDto;
 import com.sdw.recipe_platform.dto.RecipeDto;
 import com.sdw.recipe_platform.dto.RecipeResponseDto;
 import com.sdw.recipe_platform.service.RecipeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/recipes")
@@ -19,5 +18,16 @@ public class RecipeController {
     @PostMapping
     public RecipeResponseDto create(@Validated @RequestBody RecipeDto dto) {
         return recipeService.create(dto);
+    }
+
+    @PostMapping("/{id}/ingredients/add")
+    public ResponseEntity<Void> addIngredient(
+            @PathVariable Long id,
+            @Validated @RequestBody AddIngredientDto dto
+            ) {
+        recipeService.addIngredient(id, dto);
+
+        return ResponseEntity.ok()
+                .build();
     }
 }
--- src/main/java/com/sdw/recipe_platform/service/RecipeService.java
+++ src/main/java/com/sdw/recipe_platform/service/RecipeService.java
@@ -5,6 +5,8 @@ import com.sdw.recipe_platform.dto.RecipeDto;
 import com.sdw.recipe_platform.dto.RecipeResponseDto;
 import com.sdw.recipe_platform.model.Ingredient;
 import com.sdw.recipe_platform.model.Recipe;
+import com.sdw.recipe_platform.model.RecipeIngredient;
+import com.sdw.recipe_platform.model.RecipeIngredientId;
 import com.sdw.recipe_platform.repository.IngredientRepository;
 import com.sdw.recipe_platform.repository.RecipeIngredientRepository;
 import com.sdw.recipe_platform.repository.RecipeRepository;
@@ -36,7 +38,20 @@ public class RecipeService {
                 .orElseThrow(() -> new NoSuchElementException("레시피를 찾을 수 없습니다."));
         Ingredient ingredient = ingredientRepository.findById(dto.getIngredientId())
                 .orElseThrow(() -> new NoSuchElementException("재료를 찾을 수 없습니다."));
+
+        RecipeIngredientId id = new RecipeIngredientId(recipeId, ingredient.getId());
+        if (recipeIngredientRepository.existsById(id)) {
+            throw new IllegalStateException("이미 추가된 재료 입니다.");
+        }
+
+        RecipeIngredient recipeIngredient = new RecipeIngredient();
+        recipeIngredient.setId(id);
+        recipeIngredient.setRecipe(recipe);
+        recipeIngredient.setIngredient(ingredient);
+
+        recipe.getRecipeIngredients().add(recipeIngredient);
+
+        recipeRepository.save(recipe);
+
     }
-
-
 }
